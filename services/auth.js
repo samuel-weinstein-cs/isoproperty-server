42b59e4bc30c8cd268d54af14e4927cc14e0bf03
sweinstein <raeweinst@gmail.com>
2020-03-31 15:18:17 -0400

    add auth services

---
+++ services/auth.js
@@ -0,0 +1,41 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const SALT_ROUNDS = 11;
+const TOKEN_KEY = 'freshman despair';
+
+const hashPassword = async (password) => {
+  const digest = await bcrypt.hash(password, SALT_ROUNDS);
+  return digest;
+}
+
+const checkPassword = async (password, password_digest) => {
+  //console.log(typeof (password), typeof (password_digest))
+  //console.log(password.length, password_digest.length)
+  //console.log(await bcrypt.compare(password, password_digest))
+  return await bcrypt.compare(password, password_digest);
+}
+
+const genToken = (data) => {
+  const token = jwt.sign(data, TOKEN_KEY);
+  return token;
+};
+
+const restrict = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const data = jwt.verify(token, TOKEN_KEY);
+    res.locals.user = data;
+    next();
+  } catch(e){
+    console.error(e);
+    res.status(403).send('Unauthorized');
+  }
+}
+
+module.exports = {
+  hashPassword,
+  checkPassword,
+  genToken,
+  restrict
+}
