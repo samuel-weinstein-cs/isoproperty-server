855346a44670d54eec1f25fc9e800fd8a6d8cb2e
sweinstein <raeweinst@gmail.com>
2020-03-31 17:26:32 -0400

    add agent auth

--- routes/agentRouter.js
+++ routes/agentRouter.js
@@ -31,7 +31,7 @@ agentRouter.get("/", async (req, res) => {
   }
 })
 
-agentRouter.post('/' ,restrict, async (req, res, next) => {
+agentRouter.post('/' , restrict, async (req, res, next) => {
   try {
     const { name, description, email, phone, mobile } = req.body;
     const agents = await Agent.findAll({ where: { name } });
@@ -51,6 +51,70 @@ agentRouter.post('/' ,restrict, async (req, res, next) => {
       res.status(403).send('Agent already exists');
     }
   } catch (e) {
+    console.error(e);
+    next(e);
+  }
+})
+
+agentRouter.post('/login', async (req, res) => {
+  try {
+    const agent = await Agent.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+    if(agent && await checkPassword(req.body.password, agent.password_digest)) {
+      const respData = buildAuthResponse(agent);
+      res.json(respData);
+    } else {
+      res.status(401).send('Invalid Credentials');
+    }
+  } catch (e){
+    console.error(e);
+    next(e);
+  }
+})
+
+agentRouter.put('/:id', restrict, async (req, res, next) => {
+  try {
+    const agentId = res.locals.agent.id;
+    const id = req.params.id;
+    if(id==agentId){
+      const {
+        name,
+        description,
+        email,
+        phone,
+        mobile,
+        password
+      } = req.body;
+      const response = {
+        name,
+        description,
+        email,
+        phone,
+        mobile
+      }
+      let data = {
+        name,
+        description,
+        email,
+        phone,
+        mobile
+      };
+      if(password){
+        console.log("setting password");
+        const password_digest = await hashPassword(req.body.password);
+        data={...data, password_digest};
+      }
+      const agent = await Agent.findByPk(id);
+      await agent.update(data);
+      res.json(response)
+
+    } else {
+      res.status(403).send('Unauthorized');
+    }
+  } catch(e) {
     console.error(e);
     next(e);
   }
--- services/auth.js
+++ services/auth.js
@@ -25,7 +25,7 @@ const restrict = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     const data = jwt.verify(token, TOKEN_KEY);
-    res.locals.user = data;
+    res.locals.agent = data;
     next();
   } catch(e){
     console.error(e);
