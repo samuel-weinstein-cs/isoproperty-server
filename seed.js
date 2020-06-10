6cdcd07d7a3c4bb1fcec942fbbe705b7d5d38e69
samuel-weinstein-cs <raeweinst@gmail.com>
2020-06-10 16:22:29 -0400

    fix seed data

--- seed.js
+++ seed.js
@@ -10,7 +10,13 @@ const seed = async () => {
     text:
     `ISO Property's approach to business is unique. It's not just about buying or selling real estate; it is about trust, it is about the way we conduct our business that makes us different from our competitors and makes our customers return to us again and again. We are committed to providing you with a personal and professional service, underpinned by a committed approach to getting the job done with honesty and integrity.
 
-    Buying or selling your home is one of the most financially important decisions you will ever encounter. We strive to alleviate any uncertainties and concerns you have during the process. Please feel free to contact us to discuss your real estate needs.`
+    Buying or selling your home is one of the most financially important decisions you will ever encounter. We strive to alleviate any uncertainties and concerns you have during the process. Please feel free to contact us to discuss your real estate needs.
+
+    Broker cooperation always welcomed.
+
+    Member of the National Association of REALTORS
+    Member of New York State Association of REALTORS
+    Member of the Hudson Gateway Association of REALTORS`
   })
 
   const listing1 = await Listing.create({
